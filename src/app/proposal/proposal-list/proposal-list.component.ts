3c8772861536639d2af0315af46d9af6b10ae528
katkraus <katarina.kraus@mht.net>
2017-05-03 11:58:24 +0200

    Sort with small bug.

--- src/app/proposal/proposal-list/proposal-list.component.ts
+++ src/app/proposal/proposal-list/proposal-list.component.ts
@@ -30,8 +30,10 @@ export class ProposalListComponent implements OnInit {
         this.sortByProperties = [];
         this.sortByOrders = [];
         this.sortingIcons = { 
-            userAccount: { asc: false, sort: false },
-            manager: { asc: false, sort: false },
+            fisrtName: { asc: false, sort: false },
+            lastName: { asc: false, sort: false },
+            managerFistName: { asc: false, sort: false },
+            managerLastName: { asc: false, sort: false },
             companyProfile: { asc: false, sort: false },
             nationalWorkProfile: { asc: false, sort: false }
         };
@@ -47,52 +49,57 @@ export class ProposalListComponent implements OnInit {
     }
 
     // SORT
-    // sortByColumn(columnName: string, columnNameIcon: string) {
+    sortByColumn(columnName: string, columnNameIcon: string) {
+console.log("Sort columnName: " + columnName + " columnNameIcon: " + columnNameIcon);
+        var index = this.sortByProperties.indexOf(columnName);
+        var isInSortArray =  index !== -1 ;
 
-    //     var index = this.sortByProperties.indexOf(columnName);
-    //     var isInSortArray =  index !== -1 ;
+        if (isInSortArray) {
+            if (this.sortByOrders[index] == 'asc') {
 
-    //     if (isInSortArray) {
-    //         if (this.sortByOrders[index] == 'asc') {
+                this.sortByOrders[index] = 'desc';
 
-    //             this.sortByOrders[index] = 'desc';
+                this.sortingIcons[columnNameIcon].asc = false;
+                this.sortingIcons[columnNameIcon].sort = true;
+            }
+            else {
+                console.log("mot in isInSortArray");
+                _.pullAt(this.sortByProperties,[index]);
+                _.pullAt(this.sortByOrders,[index]);
 
-    //             this.sortingIcons[columnNameIcon].asc = false;
-    //             this.sortingIcons[columnNameIcon].sort = true;
-    //         }
-    //         else {
-                
-    //             _.pullAt(this.sortByProperties,[index]);
-    //             _.pullAt(this.sortByOrders,[index]);
+                this.sortingIcons[columnNameIcon].asc = false;
+                this.sortingIcons[columnNameIcon].sort = false;
+            }
+        } else {
+            console.log("usao u else");
+            // For one column sort:
+            this.sortByProperties = [];
+            this.sortByOrders = [];
+            this.resetSortingIcons();
+            //
 
-    //             this.sortingIcons[columnNameIcon].asc = false;
-    //             this.sortingIcons[columnNameIcon].sort = false;
-    //         }
-    //     } else {
-    //         // For one column sort:
-    //         this.sortByProperties = [];
-    //         this.sortByOrders = [];
-    //         this.resetSortingIcons();
-    //         //
+            this.sortByProperties.push(columnName);
+            this.sortByOrders.push('asc');
 
-    //         this.sortByProperties.push(columnName);
-    //         this.sortByOrders.push('asc');
+            this.sortingIcons[columnNameIcon].asc = true;
+            this.sortingIcons[columnNameIcon].sort = true;
+        }
+    }
 
-    //         this.sortingIcons[columnNameIcon].asc = true;
-    //         this.sortingIcons[columnNameIcon].sort = true;
-    //     }
-    // }
+    resetSortingIcons() {
+        console.log("reset");
+        this.sortingIcons = { 
+            fisrtName: { asc: false, sort: false },
+            lastName: { asc: false, sort: false },
+            managerFistName: { asc: false, sort: false },
+            managerLastName: { asc: false, sort: false },
+            companyProfile: { asc: false, sort: false },
+            nationalWorkProfile: { asc: false, sort: false }
+        };
+    }
 
-    // resetSortingIcons() {
-    //     this.sortingIcons = { 
-    //         userAccount: { asc: false, sort: false },
-    //         manager: { asc: false, sort: false },
-    //         companyProfile: { asc: false, sort: false },
-    //         nationalWorkProfile: { asc: false, sort: false }
-    //     };
-    // }
-
-    // evaluationSort() {
-    //   this.proposals = _.orderBy(this.proposals, this.sortByProperties, this.sortByOrders);  
-    // }    
+    evaluationSort() {
+        console.log("evaluationSort: sortByPropertie = " + this.sortByProperties + "sortByOrders = " + this.sortByOrders);
+      this.proposals = _.orderBy(this.proposals, this.sortByProperties, this.sortByOrders);  
+    }    
 }

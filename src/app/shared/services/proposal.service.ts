a1040f701ed94a575d5e8219714f98243abc554f
katkraus <katarina.kraus@mht.net>
2017-05-04 17:44:27 +0200

    commit

--- src/app/shared/services/proposal.service.ts
+++ src/app/shared/services/proposal.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import { Proposal } from '../../../shared/models/proposal.model';
-import { ProposalCriteria } from '../../../shared/models/proposal-criteria.model';
-import { PROPOSALS } from '../proposals-mock';
+import { Proposal } from '../../shared/models/proposal.model';
+import { ProposalCriteria } from '../../shared/models/proposal-criteria.model';
+import { PROPOSALS } from '../../proposal/shared/proposals-mock';
 
 @Injectable()
 export class ProposalService {
